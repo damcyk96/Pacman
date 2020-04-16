afcc18632fa24a927e002cf26632c9a8108eeac7
Damian Obajtek <damian.obajtek@gmail.com>
2020-04-16 19:30:06 +0200

    basic files

---
+++ src/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './style.css';
+
+function Header({ score }) {
+  return (
+    <div className="header">
+        <span className="left title">PACMAN</span>
+				<span className="right score">SCORE: <span className="points">{score}</span></span>
+    </div>
+  );
+}
+
+Header.defaultProps = {
+  score: 0
+}
+
+export default Header;
